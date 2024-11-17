263badb18cc16dfc92db84741ba157faad445a27
Matthias Huebscher <mhuebsch@umich.edu>
2024-11-17 14:22:03 -0500

    add capstone dataset class

---
+++ src/capstone_dataset.py
@@ -0,0 +1,62 @@
+import os
+
+import cv2
+import pandas as pd
+import numpy as np
+
+import project_utils as utl
+
+
+class CapstoneDataset(Dataset):
+    def __init__(self, metadata_file, image_dir, ds_type="train", transforms=None, target_transforms=None):
+        df = pd.read_csv(metadata_file)
+        
+        moas = df["Image_Metadata_MoA"].unique()
+        moas = moas[~pd.isnull(moas)]
+
+        self.class_dir = dict(zip(np.sort(moas), range(len(moas))))
+        self.classes = self.class_dir.keys()
+        
+        df_train, df_val, df_test = utl.create_train_test_val_sets(df)
+        if ds_type == "train":
+            self.metadata = df_train
+        elif ds_type == "val":
+            self.metadata = df_val
+        elif ds_type == "test":
+            self.metadata = df_test
+        elif ds_type == "full":
+            self.metadata = df[df["Image_Metadata_MoA"].notna()]
+        else:
+            self.metadata = df_test
+
+        self.image_dir = image_dir
+        self.transforms = transforms
+        self.target_transforms = target_transforms
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        img_dapi_path = os.path.join(
+            self.image_dir, self.metadata.iloc[idx, 8], self.metadata.iloc[idx, 2]
+        )
+        img_tubulin_path = os.path.join(
+            self.image_dir, self.metadata.iloc[idx, 8], self.metadata.iloc[idx, 4]
+        )
+        img_actin_path = os.path.join(
+            self.image_dir, self.metadata.iloc[idx, 8], self.metadata.iloc[idx, 6]
+        )
+
+        img_dapi = cv2.imread(img_dapi_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+        img_tubulin = cv2.imread(img_tubulin_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+        img_actin = cv2.imread(img_actin_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+        
+        image = (cv2.merge([img_dapi, img_tubulin, img_actin]))
+        image /= 255.0
+        
+        if self.transforms:
+            image = self.transforms(image)
+
+        label = self.class_dir[self.metadata.iloc[idx, 13]]
+
+        return image, label
