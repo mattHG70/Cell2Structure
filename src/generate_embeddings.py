75c73517af9ae8915f395612ec751b58e4730ec7
Matthias Huebscher <mhuebsch@umich.edu>
2024-11-29 11:07:01 -0500

    add embedding generation

---
+++ src/generate_embeddings.py
@@ -0,0 +1,194 @@
+import os
+import cv2
+import argparse
+import logging
+
+import pandas as pd
+import numpy as np
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+import torchvision
+from torchvision import transforms
+from torchvision.transforms import v2
+import torchvision.models as models
+from torchvision.models.feature_extraction import create_feature_extractor
+
+import project_utils as utl
+
+
+parser = argparse.ArgumentParser(description="Generate image embedding vectors")
+parser.add_argument("-model_path", 
+                    type=str, 
+                    required=True, 
+                    help="Path to pretrained model")
+parser.add_argument("-dataset_path", 
+                    type=str, 
+                    required=True, 
+                    help="Path to detaset csv")
+parser.add_argument("-image_path", 
+                    type=str, 
+                    required=True, 
+                    help="Path to the images")
+parser.add_argument("-output_path", 
+                    type=str, 
+                    required=True, 
+                    help="Output path to store the embedding vectors")
+parser.add_argument("-bit_depth", 
+                    type=int, 
+                    required=False, 
+                    help="Image bit depth")
+args = parser.parse_args()
+
+
+class CapstoneDataset(Dataset):
+    def __init__(self, metadata_file, image_dir, bit_depth=8, ds_type="train", transforms=None, target_transforms=None):
+        df = pd.read_csv(metadata_file)
+
+        moas = df["Image_Metadata_MoA"].unique()
+        moas = moas[~pd.isnull(moas)]
+
+        self.class_dir = dict(zip(np.sort(moas), range(len(moas))))
+        self.classes = self.class_dir.keys()
+
+        df_train, df_val, df_test = utl.create_train_test_val_sets(df)
+        if ds_type == "train":
+            self.metadata = df_train
+        elif ds_type == "val":
+            self.metadata = df_val
+        elif ds_type == "test":
+            self.metadata = df_test
+        elif ds_type == "full":
+            self.metadata = df[df["Image_Metadata_MoA"].notna()]
+        else:
+            self.metadata = df_test
+
+        self.image_dir = image_dir
+        self.transforms = transforms
+        self.target_transforms = target_transforms
+        self.bit_depth = bit_depth
+
+    def __len__(self):
+        return len(self.metadata)
+
+    def __getitem__(self, idx):
+        img_dapi_path = os.path.join(
+            self.image_dir, self.metadata.iloc[idx, 8], self.metadata.iloc[idx, 2]
+        )
+        img_tubulin_path = os.path.join(
+            self.image_dir, self.metadata.iloc[idx, 8], self.metadata.iloc[idx, 4]
+        )
+        img_actin_path = os.path.join(
+            self.image_dir, self.metadata.iloc[idx, 8], self.metadata.iloc[idx, 6]
+        )
+
+        image = None
+        if self.bit_depth == 8:
+            img_dapi = cv2.imread(img_dapi_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+            img_tubulin = cv2.imread(img_tubulin_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+            img_actin = cv2.imread(img_actin_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+
+            image = (cv2.merge([img_dapi, img_tubulin, img_actin]))
+            image /= 255.0
+        elif self.bit_depth == 16:
+            img_dapi = cv2.imread(img_dapi_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
+            img_tubulin = cv2.imread(img_tubulin_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
+            img_actin = cv2.imread(img_actin_path, cv2.IMREAD_UNCHANGED).astype(np.float32)
+
+            image = (cv2.merge([img_dapi, img_tubulin, img_actin]))
+            image /= 65535.0
+        else:
+            img_dapi = cv2.imread(img_dapi_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+            img_tubulin = cv2.imread(img_tubulin_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+            img_actin = cv2.imread(img_actin_path, cv2.IMREAD_GRAYSCALE).astype(np.float32)
+
+            image = (cv2.merge([img_dapi, img_tubulin, img_actin]))
+            image /= 255.0
+
+        if self.transforms:
+            image = self.transforms(image)
+
+        # label = self.class_dir[self.metadata.iloc[idx, 13]]
+
+        return image, -9
+
+
+def load_model(path, device, original_model=False):
+    if original_model:
+        pt_model = models.inception_v3(weights=models.Inception_V3_Weights.DEFAULT)
+        num_ftrs = pt_model.fc.in_features
+        pt_model.fc = torch.nn.Linear(num_ftrs, 13)
+    else:
+        pt_model = models.inception_v3(weights=models.Inception_V3_Weights.DEFAULT)
+        if device == "cpu":
+            pretrained_weights = torch.load(path, map_location=torch.device("cpu"))
+        else:
+            pretrained_weights = torch.load(path)
+        num_ftrs = pt_model.fc.in_features
+        pt_model.fc = torch.nn.Linear(num_ftrs, 13)
+        pt_model.load_state_dict(pretrained_weights, assign=True)
+
+
+    embd_node = {"avgpool": "avgpool"}
+
+    return create_feature_extractor(pt_model, return_nodes=embd_node)
+
+
+def main():
+    # basic logging
+    # logging.basicConfig(filename="/home/huebsma1/siads699/generate_embeddings.log",
+                        # format='%(message)s',
+                        # filemode='w')
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    batch_size = 32
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.debug(f"Use device: {device}")
+    logger.debug(f"Load model: {args.model_path}")
+
+    model = load_model(args.model_path, device, original_model=False)
+
+    transforms = v2.Compose([v2.ToImage(),
+                            v2.Resize((1024, 1024)), 
+                            v2.ToDtype(torch.float32, scale=True),
+                            v2.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    embd_dataset = CapstoneDataset(args.dataset_path,
+                                   args.image_path,
+                                   ds_type="full",
+                                   bit_depth=args.bit_depth,
+                                   transforms=transforms,
+                                   target_transforms=None)
+
+    embd_dataloader = DataLoader(embd_dataset, batch_size=batch_size, shuffle=False)
+
+    model.to(device)
+    model.eval()
+
+    logger.debug("Start processing images")
+    images_processed = 0
+    embd_vecs = np.empty((0, 2048))
+
+    with torch.no_grad():
+        for i, (images, labels) in enumerate(embd_dataloader):
+            images = images.to(device)
+            output = model(images)
+            embd_batch = output["avgpool"].detach().cpu()
+            embd_vecs = np.append(embd_vecs, embd_batch.flatten(start_dim=1).numpy(), axis=0)
+            images_processed += len(images)
+
+            if i % 20 == 0:
+                logger.debug(f"{str(images_processed)} images processed")
+
+    logger.debug("Finish processing images")
+
+    np.save(args.output_path, embd_vecs, allow_pickle=False)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/run_embd_generation.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Slurm script to run the image embedding generation job.
+# Uses one node + one gpu
+# Slurm PARAMETERS
+#SBATCH --job-name=team6_embd_gen
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=32g
+#SBATCH --account=siads699f24_class
+#SBATCH --mail-user mhuebsch@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks-per-gpu=1
+#SBATCH --time=04:00:00
+#SBATCH --output=/home/%u/siads699/%x-%j.log
+#SBATCH --error=/home/%u/siads699/error-%x-%j.log
+
+# run image embedding creation as a cluster job on Great Lakes HPC
+# using 1 node and 1 gpu
+
+# run bashrc
+source /home/mhuebsch/.bashrc
+# switch to directory containing all scripts
+cd /home/mhuebsch/siads699/Cell2Structure/src
+# load a python+conda module if necessary
+# module load <ptyhon module>
+module load cuda cudnn
+# activate custom conda environment
+mamba activate capstone
+
+# export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+START=`date +%s`; STARTDATE=`date`;
+echo [INFO] [$START] [$STARTDATE] [$$] [$JOB_ID] Starting the workflow
+echo [INFO] [$START] [$STARTDATE] [$$] [$JOB_ID] We got the following cores: $CUDA_VISIBLE_DEVICES
+
+# set device to "cuda" to enable gpu usage
+python generate_embeddings.py -dataset_path ../BBBC021_final_original_dataset.csv -model_path ../data_augment/models/capstone_model_32_25_0.0001_1_16.pt -bit_depth 16
+
+EXITCODE=$?
+
+END=`date +%s`; ENDDATE=`date`
+echo [INFO] [$END] [$ENDDATE] [$$] [$JOB_ID] Workflow finished with code $EXITCODE
+echo [INFO] [$END] [`date`] [$$] [$JOB_ID] Workflow execution time \(seconds\) : $(( $END-$START ))
